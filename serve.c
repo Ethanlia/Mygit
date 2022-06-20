f2c6e9f1da2a63bb0efa67cf8932a1adcb0ab3de
Ethanlia <ethan.liang@quectel.com>
2022-06-20 11:38:34 +0800

    增加了连续向client端发送数据功能

--- serve.c
+++ serve.c
@@ -4,10 +4,13 @@
 #include <unistd.h>
 #include <errno.h>
 #include <unistd.h>
-
 #include <sys/types.h>
+
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <sys/time.h>
 
 #include <time.h>
 
@@ -25,7 +28,7 @@ int main(int argc, char *argv[])
     struct sockaddr_in client_addr;
     int sin_size;
     
-    listenfd = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
     if(listenfd < 0)
     {
         printf("create socket error :%s %d",strerror(errno), errno);
@@ -37,7 +40,9 @@ int main(int argc, char *argv[])
     server_addr.sin_addr.s_addr=htonl(INADDR_ANY);
     server_addr.sin_port=htons(9980);
     
-    if(bind(listenfd, (struct sockaddr *)(&server_addr), sizeof(server_addr)) == -1)
+    // 绑定
+    socklen_t  addrlen  = sizeof(struct sockaddr_in);
+    if(bind(listenfd, (struct sockaddr *)(&server_addr), addrlen) == -1)
     {
         printf("bind error:%s %d",strerror(errno), errno);exit(0);            
     }
@@ -47,27 +52,88 @@ int main(int argc, char *argv[])
         printf("listen error:%s %d",strerror(errno), errno);
         exit(0);
     }
+
+    connfd = accept(listenfd, (struct sockaddr *)&client_addr, &addrlen );
+    if(connfd == -1)
+    {
+        printf("accept error:%s %d",strerror(errno), errno);
+        exit(0);        
+    }  
+     //  当链接成功 则返回一个已经链接的套接字 
+    printf("链接成功!!!\n");
     
+    char * msg =  calloc(1, 128);
+
     while (1)
     {
-        connfd = accept(listenfd, NULL, NULL );
-        if(connfd == -1)
-        {
-            printf("accept error:%s %d",strerror(errno), errno);
-            exit(0);        
-        }  
         ticks = time(NULL);
         snprintf( buff, QUECTEL_TIME_MAX_LEN, "%s\r\n", ctime(&ticks) );
         printf("buff=%s\r\n",buff);
 
-        recv(connfd,buff_msg,QUECTEL_TIME_MAX_LEN,0);
+        // 把已连接套接字 + 标准输入描述符添加到 集合中
+        fd_set set ; 
+        FD_ZERO(&set);  // 清空集合
+        FD_SET(connfd , &set); // 添加 套记字到集合中
+        FD_SET(STDIN_FILENO , &set); // 添加标准输入到集合中
+
+        // 找到描述符最大值
+        int max_fd = connfd > STDIN_FILENO ? connfd : STDIN_FILENO ;
+
+        struct timeval timeout = {
+            .tv_sec = 5 ,
+            .tv_usec = 0 
+        }; // 设置时间结构体为5秒
+
+        // 设置多路复用 监听标准输入以及已链接套接字   ， 超时设置为5秒
+        int ret_val = select( max_fd + 1 , &set, NULL , NULL , &timeout);
+        if (0 == ret_val)  // 超时
+        {
+            printf("超时警告！！！\n");
+            continue ;
+        }else if (-1 == ret_val) // 出错
+        {
+            perror("select error>>>>");
+            continue ;
+        }
+        else{
+            printf("有数据到达...\n");
+        }
+
+        // 检查哪个文件的数据到达
+        if( FD_ISSET(connfd , &set))
+        {
+    
+            ret_val = recv(connfd , msg , 128 , 0); // 从套接字获得数据并发送
+            if (0 ==  ret_val)
+            {
+                printf("客户他，，，挂了》》》》\n");
+                connfd = accept(listenfd , (struct sockaddr *)&client_addr, &addrlen);
+                if ( -1 == connfd )
+                {
+                    perror("accept error>>>");
+                    continue ; 
+                }
+                //  当链接成功 则返回一个已经链接的套接字 
+                printf("链接成功!!!\n");
+            }
+            printf("msg:%s \n" , msg);
+        }
+        
+        if( FD_ISSET(STDIN_FILENO , &set))
+        {
+            fgets(msg , 128 , stdin);  // 获取标准输入数据
+            send(connfd , msg , strlen(msg), 0 ); // 发送数据
+        }
+       /*  recv(connfd,buff_msg,QUECTEL_TIME_MAX_LEN,0);
         printf("%s",buff_msg);   
         
         write(connfd, buff, strlen(buff));
         close(connfd);
-        connfd = -1;                                                                                                                                                                                                  
+        connfd = -1;   */                                                                                                                                                                                                
     }
     
+    close(connfd);
+    close(listenfd);
     return 0;
  
 }
